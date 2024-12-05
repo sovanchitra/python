2b9f220c1bb7820e93f213135e26f3a24670edce
sovanchitra <chivsovanchetra@gmail.com>
2024-12-05 11:16:01 +0700

    first commit

---
+++ 22.Library and Custom Module/cal_n.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def sum_(n):
+    return n * (n + 1) // 2
+
+def sum2_(n):
+    t = 0
+    for i in range(1, n + 1):
+        t = t + i
+    return t
+
+
+# In[ ]:
+
+
+
+
---
+++ 22.Library and Custom Module/fund_cal.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### user defined module for funding with ticket
+
+# In[2]:
+
+
+def fund(t):
+    return t + 3
+
+
+# In[ ]:
+
+
+
+
